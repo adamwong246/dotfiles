3974b65b8e2c13ffe2f0e74721827e88441f90d2
Adam Wong <adamwong246@gmail.com>
2014-12-05 12:00:26 -0800

    add some echo statements

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo 'getting dependicies'
 git subtree add --prefix=.zsh_plugins/colored-man https://github.com/adamwong246/heck-yeah-zsh-colored-man.git master
 
+echo 'stow it!'
 stow . --ignore='.DS_Store install.sh README.md'
