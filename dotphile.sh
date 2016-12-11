b3b1c42043cd2f41177b03cd7cd9d29f2b8fc77c
adam wong <adamwong246@gmail.com>
2016-12-10 17:06:45 -0800

    no message

--- dotphile.sh
+++ dotphile.sh
@@ -14,53 +14,28 @@ fi
 
 if [ $# -eq 1 ]; then
   pile=$1
-  echo "unpacking ${pile}..."
   files="$(find -L "$pile" -type f ! -name '*.DS_Store')"
   for file in $files; do
     base=${file#$pile/}
     seed=~/${PWD##*/}/${file}
     target=${HOME}/${base}
-
-    if [ -L ${target} ] ; then # file is good or bad symlink?
-
-      readlink="$(readlink $target)"
-      if [ $readlink = "$seed" ]; then
+    if ! [ -e "$target" ]; then
+      ln -sv ~/${PWD##*/}/${file} ${target}
+    else
+      if [ -L ${target} ] && [ $(readlink $target) = "$seed" ]; then
         echo "${base} ✓"
-      else
-        echo "${base} ✗ ${seed} is symlinked to $(readlink $target), which is not managed by dotphile."
-        echo "a) add $(readlink $target) to ${pile} as ${seed},"
-        echo "   git-commit the changes made to ${base},"
-        echo "   and finally overwriting ${base} by forcibly symlinking ${file} to ${target}"
+      elif [ -L ${target} ]; then
+        echo "${target} already exists"
+        echo "a) add $target to ${pile}"
         echo "b) overwrite ${base} by forcibly symlinking ${file} to ${target}"
         echo "c) do nothing"
-
         read -r -p "What to do? [a/b/*]" response
         case $response in
           [a]) sh dotphile.sh $pile $target ;;
           [b]) ln -svf ~/${PWD##*/}/${file} ${target} ;;
-          *) echo "something else" ;;
+          *) echo "!?" ;;
         esac
-
       fi
-    elif [ -e ${target} ] ; then # file is not a symlink?
-      echo "${target} already exists"
-      echo "a) add $target to ${pile} as ${seed},"
-      echo "   git-commit the changes made to ${base},"
-      echo "   and finally overwriting ${base} by forcibly symlinking ${file} to ${target}"
-      echo "b) overwrite ${base} by forcibly symlinking ${file} to ${target}"
-      echo "else do nothing"
-
-      read -r -p "What to do? [a/b/*]" response
-      case $response in
-        [a]) sh dotphile.sh $pile $target ;;
-        [b]) ln -svf ~/${PWD##*/}/${file} ${target} ;;
-        *) echo "something else" ;;
-      esac
-
-    else # file is missing!
-      echo "${target} doesn't exist. Symlinking ${target} to ${seed}"
-      ln -sv ~/${PWD##*/}/${file} ${target}
-      echo "${base} ✓"
     fi
   done
 fi
@@ -70,27 +45,16 @@ if [ $# -eq 2 ]; then
   file=$2
   relativeSeed="./${pile}/${file#$HOME/}"
   absoluteSeed=~/${PWD##*/}/${pile}/${file#$HOME/}
-
   echo "adding ${file} to ${pile} as ${relativeSeed}..."
-
-  if [ -L ${file} ] ; then # file is good or bad symlink?
-    if [ "$(readlink $file)" = "$absoluteSeed" ]; then
-      echo "${relativeSeed} ✓"
-    else
-      echo "it's a symlink but it doesn't match: ${absoluteSeed}"
-    fi
+  if [ -L ${file} ] && [ "$(readlink $file)" = "$absoluteSeed" ]; then
+    echo "${relativeSeed} ✓"
   elif [ -e ${file} ] ; then # file is not a symlink?
-     echo "${file} exists and it isn't a symlink"
      mkdir -p ./${pile}
      cp ${file} ${pile}
-
-     echo "adding ${relativeSeed} to git. ${file} *will* be forcefully overwritten!"
+     echo "adding ${relativeSeed} to git. ${file} *will* be overwritten after staging changes!"
      git add -i $relativeSeed
-
-     echo "forcefully symlinking ${relativeSeed} back to ${file}"
-     ln -sf $absoluteSeed ${file}
+     ln -sfv $absoluteSeed ${file}
   else
     echo "${file} doesn't exist!"
   fi
-
 fi
